8df1d148823dea8a24640ff42b6081296930bb2c
rim-code <friouirim@gmail.com>
2019-12-10 14:34:09 +0100

    Updates

---
+++ precache-manifest.f4a9e34f478c56c7455e13ca76d6c1f9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "476e688b8694feca65add63b1f33f7ea",
+    "url": "/cardreact/index.html"
+  },
+  {
+    "revision": "64865e34d93f137e759d",
+    "url": "/cardreact/static/css/main.72245cd0.chunk.css"
+  },
+  {
+    "revision": "a15d3a7139d6b65d72c1",
+    "url": "/cardreact/static/js/2.a17b3240.chunk.js"
+  },
+  {
+    "revision": "64865e34d93f137e759d",
+    "url": "/cardreact/static/js/main.52602cc2.chunk.js"
+  },
+  {
+    "revision": "69d29bb3e4512c691134",
+    "url": "/cardreact/static/js/runtime-main.5ec7e3a8.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/cardreact/precache-manifest.f4a9e34f478c56c7455e13ca76d6c1f9.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/cardreact/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
